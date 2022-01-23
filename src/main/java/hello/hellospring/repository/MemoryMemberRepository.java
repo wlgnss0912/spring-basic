5b101dc5b930024000e639f4b208ffb58879a2ea
wlgnss0912 <jeonjihoon93@naver.com>
2022-01-23 23:12:51 +0900

    spring 22.01.23 MVC 패턴으로 작성하기

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,7 +14,6 @@ public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
-    @Override
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
